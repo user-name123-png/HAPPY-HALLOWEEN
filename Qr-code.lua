0a97340cebe62e46eff216f1b7a98cc86a4a30ec
user-name123-png <mgkiller5114.ez@gmail.com>
2025-10-23 22:26:49 +0700

    Update and rename Qr-code to Qr-code.lua

---
+++ Qr-code.lua
@@ -0,0 +1,5 @@
+getgenv = getgenv or function() return _G end
+getgenv().Configuration = {
+    Key = ""
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/user-name123-png/HAPPY-HALLOWEEN/refs/heads/main/Palindrome.lua"))()
